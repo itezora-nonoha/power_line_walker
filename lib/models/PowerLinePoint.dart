05275b0b8ac1d73c5454b41e08b9502196da4cd6
itezora-nonoha <itezora_nonoha@gmail.com>
2023-08-16 13:37:00 +0900

    add PowerLinePointを追加

---
+++ lib/models/PowerLinePoint.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PowerLinePoint {
+  // double _latitude;
+  // double _longitude;
+  final LatLng latlng;
+  final String names;
+  // String note;
+  final DateTime createdAt;
+
+  PowerLinePoint({required this.latlng, required this.names, required this.createdAt});
+
+  factory PowerLinePoint.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data();
+    return PowerLinePoint(
+        latlng: LatLng(data?['latitude'], data?['longitude']),
+        names: data?['names'],
+        createdAt: data?['createdAt'].toDate()
+      );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "latitude": latlng.latitude,
+      "longitude": latlng.longitude,
+      "names": names,
+      "createdAt": createdAt,
+    };
+  }
+  // String getFirstNames(){
+  //   return this.names[0];
+  // }
+}
