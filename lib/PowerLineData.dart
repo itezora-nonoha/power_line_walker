75abef44be147b08d793ea7cbbca43065abff608
itezora-nonoha <itezora_nonoha@gmail.com>
2023-08-16 12:40:28 +0900

    fix PowerLineDataの調整

--- lib/PowerLineData.dart
+++ lib/PowerLineData.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -20,28 +20,28 @@ class PowerLineData {
     return "complete";
   }
 
-Future<void>get() async {
-//     const lat = '35.65138';
-//     const lon = '139.63670';
-//     const key = 'aa50ce0598c22de20f75cf3ea31ac312';
+// Future<void>get() async {
+// //     const lat = '35.65138';
+// //     const lon = '139.63670';
+// //     const key = 'aa50ce0598c22de20f75cf3ea31ac312';
 
-    const domain = 'https://script.googleapis.com';
-    const pass = '/v1/scripts/AKfycbyBc4K0t2n_6itPUyZASY4tWiX359YRuSLb-bGS4Gqkvtb-nSfx7jOm40Y5uoBn_t1nBg:run';
-    const payload = {
-      "function": 'getActiveSheetName',
-      // parameters": ['Hello, world!']
-    };
+//     const domain = 'https://script.googleapis.com';
+//     const pass = '/v1/scripts/AKfycbyBc4K0t2n_6itPUyZASY4tWiX359YRuSLb-bGS4Gqkvtb-nSfx7jOm40Y5uoBn_t1nBg:run';
+//     const payload = {
+//       "function": 'getActiveSheetName',
+//       // parameters": ['Hello, world!']
+//     };
 
 
-//     const query = '?lat=$lat&lon=$lon&exclude=daily&lang=ja&appid=$key';
-    // var url = Uri.parse(domain + pass + query);
-    var url = Uri.parse(domain + pass);
-//     debugPrint('url: $url');
-    final response = await http.post(url, body: {
-      'function': 'getActiveSheetName',
-    });
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+// //     const query = '?lat=$lat&lon=$lon&exclude=daily&lang=ja&appid=$key';
+//     // var url = Uri.parse(domain + pass + query);
+//     var url = Uri.parse(domain + pass);
+// //     debugPrint('url: $url');
+//     final response = await http.post(url, body: {
+//       'function': 'getActiveSheetName',
+//     });
+//     print('Response status: ${response.statusCode}');
+//     print('Response body: ${response.body}');
 
     // HttpClient client = new HttpClient();
     // HttpClientRequest request = await client.postUrl(url,);
@@ -66,7 +66,7 @@ Future<void>get() async {
 //       return model;
 //     }
 //     return null;
-}
+// }
 
   Map<String, dynamic> getPoints(){
     return map;
@@ -77,5 +77,5 @@ Future<void>get() async {
 
 void main(){
   PowerLineData powerLineData = PowerLineData();
-  powerLineData.get();
+  // powerLineData.get();
 }
