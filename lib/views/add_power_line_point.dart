7b7a44c8482fcdc630720bbe100436de8bedf5d6
itezora-nonoha <itezora_nonoha@gmail.com>
2023-08-20 00:57:56 +0900

    update 地点登録画面のボタンおよびテキストボックスのレイアウトを調整

--- lib/views/add_power_line_point.dart
+++ lib/views/add_power_line_point.dart
@@ -1,9 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:power_line_walker/db/PowerLinePointHelper.dart';
 import 'package:power_line_walker/models/PowerLinePoint.dart';
-import 'package:power_line_walker/views/power_line_map.dart';
+import 'package:power_line_walker/widgets/point_type_selector_box.dart';
 
 class AddPowerLinePoint extends StatelessWidget {
   // AddPowerLinePoint({super.key, required this.title, this.latlng});
@@ -21,8 +20,8 @@ class AddPowerLinePoint extends StatelessWidget {
   final TextEditingController _controllerLatitude = TextEditingController();
   final TextEditingController _controllerLongitude = TextEditingController();
   final TextEditingController _controllerName = TextEditingController();
+  final PointTypeSelectorBox _pointTypeSelectorBox = PointTypeSelectorBox();
   List<PowerLinePoint> _powerLinePointList = [];
-  
 
   // @override
   // void initState() {
@@ -40,25 +39,23 @@ class AddPowerLinePoint extends StatelessWidget {
   //   super.dispose();
   // }
 
-  void _addPowerLinePoint(LatLng latlng, String names) async {
-    final PowerLinePoint powerLinePoint =
-        PowerLinePoint(latlng: latlng, names: names, createdAt: DateTime.now());
-    await PowerLinePointHelper.instance.insert(powerLinePoint);
-    _powerLinePointList.add(powerLinePoint);
-    print(powerLinePoint);
-    // setState(() {});
-  }
-
-  void _saveButtonPushed(){
+  void _saveButtonPushed() {
     var latitude = double.parse(_controllerLatitude.text);
     var longitude = double.parse(_controllerLongitude.text);
-    var latLng = LatLng(latitude, longitude);
+    var latlng = LatLng(latitude, longitude);
     var name = _controllerName.text;
     // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
     //   content: Text('OnTapped: ${name}, ${latLng.toString()}'),
     //   duration: const Duration(seconds: 1),
     // ));
-    _addPowerLinePoint(latLng, name);
+    _addPowerLinePoint(latlng, name);
+  }
+
+  void _addPowerLinePoint(LatLng latlng, String names) async {
+    final PowerLinePoint powerLinePoint =
+        PowerLinePoint(latlng: latlng, names: names, createdAt: DateTime.now());
+    await PowerLinePointHelper.instance.insert(powerLinePoint);
+    _powerLinePointList.add(powerLinePoint);
   }
 
   @override
@@ -69,178 +66,59 @@ class AddPowerLinePoint extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: SafeArea(
-            child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-              Expanded(
-                  child: TextField(
-                controller: _controllerLatitude,
-                autofocus: false,
-                decoration: const InputDecoration(labelText: 'latitude'),
-              )),
-              Expanded(
-                  child: TextField(
-                controller: _controllerLongitude,
-                autofocus: false,
-                decoration: const InputDecoration(labelText: 'longitude'),
-              )),
-              Expanded(
-                  child: TextField(
+        body: Container(
+            margin: const EdgeInsets.all(8),
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Expanded(
+                    child: TextField(
+                  controller: _controllerLatitude,
+                  autofocus: false,
+                  decoration: const InputDecoration(labelText: 'latitude'),
+                )),
+                const SizedBox(width: 10, child: Spacer()),
+                Expanded(
+                    child: TextField(
+                  controller: _controllerLongitude,
+                  autofocus: false,
+                  decoration: const InputDecoration(labelText: 'longitude'),
+                )),
+              ]),
+              TextField(
                 controller: _controllerName,
                 autofocus: false,
                 decoration: const InputDecoration(labelText: 'name'),
                 onSubmitted: (String value) {
-                  double latitude = double.parse(_controllerLatitude.text);
-                  double longitude = double.parse(_controllerLongitude.text);
-                  LatLng latlng = LatLng(latitude, longitude);
-                  print(latlng);
-                  _addPowerLinePoint(latlng, _controllerName.text);
-                  _controllerLatitude.clear();
-                  _controllerLongitude.clear();
-                  _controllerName.clear();
+                  _saveButtonPushed();
                 },
-              )),
-              SizedBox(
-                width: 50,
-                height: 25,
-                child: ElevatedButton(
-                    onPressed: () {
-                      _saveButtonPushed();
-                      // _getPowerLinePoint(powerLinePoint.names);
-                    },
-                    child: const Icon(
-                      Icons.book,
-                      size: 15,
-                    )),
               ),
+              _pointTypeSelectorBox,
+              Container(
+                  margin: const EdgeInsets.all(8),
+                  child: SizedBox(
+                    width: 100,
+                    // width: double.infinity,
+                    height: 30,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _saveButtonPushed();
+                        },
+                        child: const Text("地点登録")
+                        // const Icon(Icons.book,size: 15),
+                        ),
+                  )),
             ])));
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text(widget.title),
-  //     ),
-  //     body: SafeArea(
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //         children: [
-  //           Expanded(
-  //             child: SizedBox(
-  //               child: _powerLinePointList.isEmpty
-  //                   ? const Center(child: Text('NO DATA'))
-  //                   : ListView.builder(
-  //                       itemCount: _powerLinePointList.length,
-  //                       itemBuilder: (BuildContext context, int index) {
-  //                         final powerLinePoint = _powerLinePointList[index];
-  //                         return Card(
-  //                           child: InkWell(
-  //                             child: Padding(
-  //                               padding: const EdgeInsets.all(8.0),
-  //                               child: Row(
-  //                                 children: <Widget>[
-  //                                   Expanded(
-  //                                     child: Text(
-  //                                       powerLinePoint.names,
-  //                                       style: const TextStyle(fontSize: 16),
-  //                                     ),
-  //                                   ),
-  //                                   Expanded(
-  //                                     child: Text(
-  //                                       powerLinePoint.latlng.latitude.toString(),
-  //                                       style: const TextStyle(fontSize: 16),
-  //                                     ),
-  //                                   ),
-  //                                   Expanded(
-  //                                     child: Text(
-  //                                       powerLinePoint.latlng.longitude.toString(),
-  //                                       style: const TextStyle(fontSize: 16),
-  //                                     ),
-  //                                   ),
-  //                                   SizedBox(
-  //                                     width: 50,
-  //                                     height: 25,
-  //                                     child: ElevatedButton(
-  //                                         onPressed: () {
-  //                                           _getPowerLinePoint(powerLinePoint.names);
-  //                                         },
-  //                                         child: const Icon(
-  //                                           Icons.book,
-  //                                           size: 15,
-  //                                         )),
-  //                                   ),
-  //                                   const SizedBox(
-  //                                     width: 5,
-  //                                     height: 25,
-  //                                   ),
-  //                                   SizedBox(
-  //                                     width: 50,
-  //                                     height: 25,
-  //                                     child: ElevatedButton(
-  //                                         onPressed: () {
-  //                                           _deletePowerLinePoint(powerLinePoint.names, index);
-  //                                         },
-  //                                         child: const Icon(
-  //                                           Icons.delete,
-  //                                           size: 15,
-  //                                         )),
-  //                                   ),
-  //                                 ],
-  //                               ),
-  //                             ),
-  //                           ),
-  //                         );
-  //                       }),
-  //             ),
-  //           ),
-  //           Row(
-  //             children: [
-  //               Expanded(
-  //                   child: TextField(
-  //                 controller: _controllerLatitude,
-  //                 autofocus: false,
-  //                 decoration: const InputDecoration(labelText: 'latitude'),
-  //               )),
-  //               Expanded(
-  //                   child: TextField(
-  //                 controller: _controllerLongitude,
-  //                 autofocus: false,
-  //                 decoration: const InputDecoration(labelText: 'longitude'),
-  //               )),
-  //               Expanded(
-  //                   child: TextField(
-  //                 controller: _controllerName,
-  //                 autofocus: false,
-  //                 decoration: const InputDecoration(labelText: 'name'),
-  //                 onSubmitted: (String value) {
-  //                   double latitude = double.parse(_controllerLatitude.text);
-  //                   double longitude = double.parse(_controllerLongitude.text);
-  //                   LatLng latlng = LatLng(latitude, longitude);
-  //                   print(latlng);
-  //                   _addPowerLinePoint(latlng, _controllerName.text);_controllerLatitude.clear();
-  //                   _controllerLongitude.clear();
-  //                   _controllerName.clear();
-  //                 },
-  //               )),
-  //               ElevatedButton(
-  //                   onPressed: () {
-  //                   double latitude = double.parse(_controllerLatitude.text);
-  //                   double longitude = double.parse(_controllerLongitude.text);
-  //                   LatLng latlng = LatLng(latitude, longitude);
-  //                   print(latlng);
-  //                   _addPowerLinePoint(latlng, _controllerName.text);
-  //                   _controllerLatitude.clear();
-  //                   _controllerLongitude.clear();
-  //                   _controllerName.clear();
-  //                 },
-  //                   child: const Icon(Icons.add)),
-  //             ],
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: AddPowerLinePoint(title: 'test', latlng: const LatLng(35, 135)),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
---
+++ lib/widgets/point_type_selector_box.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PointTypeSelectorBox extends StatefulWidget {
+  const PointTypeSelectorBox({Key? key}) : super(key: key);
+
+  @override
+  State<PointTypeSelectorBox> createState() => _PointTypeSelectorBoxState();
+}
+
+class _PointTypeSelectorBoxState extends State<PointTypeSelectorBox> {
+  String isSelectedValue = 'Tower';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      items: const[
+        DropdownMenuItem(
+          value: 'Tower',
+          child: Text('鉄塔'),
+        ),
+        DropdownMenuItem(
+            value: 'Substation',
+            child: Text('変電所'),
+        ),
+        DropdownMenuItem(
+            value: 'Switchyard',
+            child: Text('開閉所'),
+        ),
+      ],
+      value: isSelectedValue,
+      onChanged: (String? value) {
+        setState(() {
+          isSelectedValue = value!;
+        });
+      },
+    );
+  }
+}
