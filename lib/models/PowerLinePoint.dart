c15a6fa0d8fce0ca16445c147bcc54a5dbed413c
itezora-nonoha <itezora_nonoha@gmail.com>
2023-08-26 15:35:27 +0900

    update PowerLinePointクラスについて、地点登録時に使用する「一意なキーの生成」ロジックを記述

--- lib/models/PowerLinePoint.dart
+++ lib/models/PowerLinePoint.dart
@@ -37,4 +37,24 @@ class PowerLinePoint {
   String toString(){
     return '$latlng ... $names';
   }
+
+  String generateUniqueKey(){
+    // 併架されている場合の実際の鉄塔表示名を取得
+    String powerPointLabel = names[0];
+    List<String> labelNameSplit = powerPointLabel.split('-');
+  
+    String labelName = labelNameSplit[0];
+    double labelNumber = double.parse(labelNameSplit[1]);
+    String labelNumberPadding = labelNumber.toString().padLeft(4, "0");
+  
+    String pointKey = '$labelName-$labelNumberPadding';
+
+    // 「送電路線名-鉄塔番号」の後ろに更に文字列がついている場合に、そのテキストを付記する
+    if (labelNameSplit.length > 2){
+      String str = '${labelNameSplit[0]}-${labelNameSplit[1]}';
+      String extraText = str.substring(pointKey.length);
+      pointKey ='$pointKey$extraText';
+    }
+    return pointKey;
+  }
 }
